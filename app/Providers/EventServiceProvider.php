d92b533198f7ece5a5fb19d16f5b9322d05c6d57
heguohui2018 <heguohui2017@outlook.com>
2020-03-09 16:18:55 +0800

    注册事件监听

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -9,26 +9,31 @@ use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array
-     */
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-    ];
+	/**
+	 * The event listener mappings for the application.
+	 *
+	 * @var array
+	 */
+	protected $listen = [
+		Registered::class => [
+			SendEmailVerificationNotification::class,
+		],
 
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        parent::boot();
+		/* 注册事件监听 */
+		\Illuminate\Auth\Events\Verified::class => [
+			\App\Listeners\EmailVerified::class,
+		],
+	];
 
-        //
-    }
+	/**
+	 * Register any events for your application.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		parent::boot();
+
+		//
+	}
 }
