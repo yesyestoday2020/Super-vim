e38d36ff84a9c515ed6dc4902e89b1c25b575223
heguohui2018 <heguohui2017@outlook.com>
2020-03-12 13:30:10 +0800

    修改命令空间错误

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 
 class UsersController extends Controller
@@ -26,5 +27,16 @@ class UsersController extends Controller
 	public function edit(User $user)
 	{
 		return view('users.edit', compact('user'));
+	}
+
+	/**
+	 * undocumented function
+	 *
+	 * @return void
+	 */
+	public function update(UserRequest $request, User $user)
+	{
+		$user->update($request->all());
+		return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
 	}
 }
