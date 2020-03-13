d4f22ae4e8e0bb0e265be3590b141794ee923c53
heguohui2018 <heguohui2017@outlook.com>
2020-03-13 11:52:45 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 /* 资源控制器 */
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
